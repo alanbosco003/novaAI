8e9a622ce5d11fb2989bf8ceedef9115247b448e
Alan <alan.bosco@lendingkart.com>
2025-02-12 16:56:02 +0530

    Updated main

--- nova_ai/lib/main.dart
+++ nova_ai/lib/main.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_ai/core/database/database_service.dart';
+import 'package:nova_ai/core/repository/album_repository.dart';
+import 'package:nova_ai/core/repository/image_repository.dart';
+import 'package:nova_ai/data/services/album_service.dart';
+import 'package:nova_ai/logic/bloc/album/album_bloc.dart';
+import 'package:nova_ai/presentation/screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  const String imageBoxName = 'imageBox';
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await DatabaseService.incrementLaunchCount(); // Track app opens
+
+  print("Launch Count after increment: ${DatabaseService.getLaunchCount()}");
+
+  runApp(const MyApp()); // Now run the app
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AlbumBloc(
+            AlbumService(
+              AlbumRepository(),
+              ImageRepository(),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: Container(),
     );
   }
 }
